1da983adf15b7964e270b1e614287641c17ad63f
victor-vilela <victor_vilel@hotmail.com>
2022-04-27 23:33:13 -0300

    function add to database

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,26 @@
-from .config import settings
+import typer
+from beerlog.core import add_beer_to_database
 
+main = typer.Typer(help="Beer Management Application")
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command("add")
+def add(
+        name:str, 
+        style:str,
+        flavor:int = typer.Option(...),
+        image:int = typer.Option(...),
+        cost:int = typer.Option(...)
+    ):
+    """
+    Adds a new beer to database
+    """
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print('Beer added to database')
+
+@main.command("list")
+def list_beers(style:str):
+    """
+    List beers in database
+    """
+    print(style)
+
---
+++ beerlog/core.py
@@ -0,0 +1,24 @@
+from typing import Optional, List
+from sqlmodel import select
+from beerlog.database import get_session
+from beerlog.models import Beer
+
+def add_beer_to_database(
+        name:str,
+        style:str,
+        flavor:int,
+        image:int,
+        cost:int,
+    ) -> bool:
+    with get_session() as session:
+        beer = Beer(
+            name=name,
+            style=style,
+            flavor=flavor,
+            image=image,
+            cost=cost
+        )
+        session.add(beer)
+        session.commit()
+
+    return True
